90f3ba1c478b4c3db76ffa65ef4e6b877e9425b8
Waqas Ahmed <wahmed@sequelmed.com>
2020-02-10 23:11:02 +0500

    fix: [WIP] MCD CCDA import changes

--- lib/health-data-standards/import/ccda/vital_sign_importer.rb
+++ lib/health-data-standards/import/ccda/vital_sign_importer.rb
@@ -3,7 +3,7 @@ module HealthDataStandards
     module CCDA
       class VitalSignImporter < CDA::ResultImporter
         def initialize
-          super(CDA::EntryFinder.new("//cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.22.4.27']"))
+          super(CDA::EntryFinder.new("//cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.22.4.27' or cda:templateId/@root='2.16.840.1.113883.3.88.11.83.14']"))
         end
       end
     end
