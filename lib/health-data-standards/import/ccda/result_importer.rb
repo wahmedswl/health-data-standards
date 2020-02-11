4fa8939bff5c85231688dff0459f75906f212e2f
Waqas Ahmed <wahmed@sequelmed.com>
2020-02-11 17:35:19 +0500

    fix: MCD, CCD import changes

--- lib/health-data-standards/import/ccda/result_importer.rb
+++ lib/health-data-standards/import/ccda/result_importer.rb
@@ -3,7 +3,7 @@ module HealthDataStandards
     module CCDA
       class ResultImporter < CDA::ResultImporter
         def initialize
-          super(CDA::EntryFinder.new("//cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.22.4.2' or cda:templateId/@root='2.16.840.1.113883.3.88.11.83.122']"))
+          super(CDA::EntryFinder.new("//cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.22.4.2' or cda:templateId/@root='1.3.6.1.4.1.19376.1.5.3.1.4.13']"))
         end
       end
     end
