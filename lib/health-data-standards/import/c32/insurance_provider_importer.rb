9f0b67050e4addf1b211fa8c179b304343f9de01
Waqas Ahmed <wahmed@sequelmed.com>
2020-07-22 16:05:53 +0500

    fix: VP Foot & Ankle payers changes

--- lib/health-data-standards/import/c32/insurance_provider_importer.rb
+++ lib/health-data-standards/import/c32/insurance_provider_importer.rb
@@ -11,6 +11,7 @@ module HealthDataStandards
 
         def create_entry(payer_element, nrh = CDA::NarrativeReferenceHandler.new)
           ip = InsuranceProvider.new
+          extract_codes(payer_element, ip)
           type = extract_code(payer_element, "./cda:code")
           ip.type = type['code'] if type
           ip.payer = import_organization(payer_element.at_xpath("./cda:performer/cda:assignedEntity[cda:code[@code='PAYOR']]"))
@@ -18,13 +19,21 @@ module HealthDataStandards
           ip.subscriber = import_person(payer_element.at_xpath("./cda:participant[@typeCode='HLD']/cda:participantRole"))
           member_info_element = payer_element.at_xpath("cda:participant[@typeCode='COV']")
           extract_dates(member_info_element, ip, "time")
-          name = payer_element.at_xpath("./cda:entryRelationship[@typeCode='REFR']/cda:act[@classCode='ACT' and @moodCode='DEF']/cda:text")
+          covered_party_element = payer_element.at_xpath("./cda:entryRelationship[@typeCode='REFR']/cda:act[@classCode='ACT' and @moodCode='DEF']")
+          ip[:covered_party_id] = covered_party_element.at_xpath("./cda:id")['extension'] if covered_party_element.at_xpath("./cda:id")
+          name = covered_party_element.at_xpath("./cda:text")
           ip.name = name.try(:text)
           if member_info_element
             patient_element = member_info_element.at_xpath("./cda:participantRole[@classCode='PAT']")
             if patient_element
               ip.member_id = patient_element.at_xpath("./cda:id")
               ip.relationship = extract_code(patient_element, "./cda:code")
+            else
+              patient_element = member_info_element.at_xpath("./cda:participantRole")
+              if patient_element
+                ip.member_id = patient_element.at_xpath("./cda:id")['extension'] if patient_element.at_xpath("./cda:id")
+                ip.relationship = extract_code(patient_element, "./cda:code")
+              end
             end
           end
           ip.financial_responsibility_type = extract_code(payer_element, "./cda:performer/cda:assignedEntity/cda:code")
